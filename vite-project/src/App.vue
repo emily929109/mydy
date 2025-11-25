2dc507e91aaa98ccad7b387a765f702de1aa4945
emily <emily929109@gmail.com>
2025-11-25 18:25:16 +0800

    vite

---
+++ vite-project/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+import DatePicker from "primevue/datepicker";
+import FloatLabel from "primevue/floatlabel";
+import InputText from "primevue/inputtext";
+
+const date = ref(null);
+</script>
+
+<template>
+  <div style="padding: 40px">
+    <FloatLabel>
+      <DatePicker id="birthday" v-model="date" />
+      <label for="birthday">生日</label>
+    </FloatLabel>
+  </div>
+</template>
+
+<style scoped></style>
