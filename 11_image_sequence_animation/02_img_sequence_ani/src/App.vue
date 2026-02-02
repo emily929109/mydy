982a426dfc4e5a0e4e89c12f0bd7a14112261c63
emily <emily929109@gmail.com>
2026-02-02 17:58:10 +0800

    fix 殘影問題、滑到播放、滑出停止、再次滑入繼續播放done

--- 11_image_sequence_animation/02_img_sequence_ani/src/App.vue
+++ 11_image_sequence_animation/02_img_sequence_ani/src/App.vue
@@ -1,50 +1,63 @@
 <script setup>
 import { ref, onMounted } from "vue";
-import gsap from "gsap";
 import parseAPNG from "apng-js";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
 const img1Src = "./images/apng_v2.png";
 
 window.onload = function () {
   const canvas = document.querySelector("#canvas");
   const ctx = canvas.getContext("2d");
+  const playerMap = new Map();
 
   //將圖片轉換為純二進位數據
   async function getImgBuffer(url) {
-    const response = await fetch(url);
-    const buffer = await response.arrayBuffer(); //(純二進位數據)
-
-    return buffer;
+    try {
+      const response = await fetch(url);
+      const buffer = await response.arrayBuffer(); //(純二進位數據)
+      return buffer;
+    } catch (error) {
+      console.error("圖片讀取失敗:", error);
+    }
   }
 
-  //apng解析、準備播放器
-  async function createApngPlayer(url, ctx, options = {}) {
+  //初始化apng
+  async function initApngPlayer(url, ctx) {
     const imgBuffer = await getImgBuffer(url);
-    const apng = parseAPNG(imgBuffer);
+    if (!imgBuffer) return;
 
-    //canvas尺寸設定為apng圖片尺寸
+    const apng = parseAPNG(imgBuffer);
     canvas.width = apng.width;
     canvas.height = apng.height;
+    apng.numPlays = 2;
+    const player = await apng.getPlayer(ctx);
+    playerMap.set(canvas, player);
 
     // Object.keys(options).forEach((key) => {
     //   apng[key] = options[key];
     // });
-    const player = await apng.getPlayer(ctx);
-    return player;
+
+    ScrollTrigger.create({
+      trigger: canvas,
+      start: "top 50%", // 視窗的 50% 碰到元素的頂部時觸發 (等同 threshold 0.5)
+      end: "bottom top",
+      onEnter: () => player.play(),
+      onLeave: () => player.pause(),
+      onLeaveBack: () => player.pause(),
+      onEnterBack: () => player.play(),
+
+      // markers: true,
+    });
   }
 
-  // createApngPlayer(img1Src, ctx);
-
-  //IIFE (立即呼叫函式表達式)
-  (async () => {
-    const player1 = await createApngPlayer(img1Src, ctx, { numPlays: 1 });
-    // const player2 = await createApngPlayer(img2Src, ctx);
-    player1.play();
-  })();
+  initApngPlayer(img1Src, ctx);
 };
 </script>
 
 <template>
+  <div style="min-height: 1200px"></div>
   <canvas id="canvas"></canvas>
   <!-- <img :src="img1Src" /> -->
 </template>
