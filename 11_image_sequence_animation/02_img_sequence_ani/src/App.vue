e51d53c2679695bdf5ab266090a000403276e8c7
emily <emily929109@gmail.com>
2026-02-13 14:49:51 +0800

    code review 1.移除dom操作 數據驅動 2.移除gsap.ticker

--- 11_image_sequence_animation/02_img_sequence_ani/src/App.vue
+++ 11_image_sequence_animation/02_img_sequence_ani/src/App.vue
@@ -11,130 +11,129 @@ const img1Src = "/images/apng_v2.png";
 const img2Src = "/images/apng_2.png";
 const img3Src = "/images/apng_3.png";
 const apngList = [img1Src, img2Src, img3Src];
-
-// 1. 定義資料陣列
 const videoItems = [
   { id: 1, text: "簡單一掃 即可付款" },
-  { id: 2, text: "繳帳單也能很輕鬆" }, // 這一筆我們希望它反轉
+  { id: 2, text: "繳帳單也能很輕鬆" },
   { id: 3, text: "快速結帳 無需等待" },
 ];
-
-// 2. 準備一個陣列來存所有的 canvas DOM (給之後的 GSAP 用)
-const canvasRefs = ref([]);
+const canvasRefs = ref([]); //供gsap使用
 
 onMounted(async () => {
-  // 1. 載入共用的手機背景圖
-  let bgImg;
-  try {
-    bgImg = await loadImage("/images/mb.png");
-  } catch (e) {
-    console.error("背景圖載入失敗", e);
-    return;
+  //  載入背景圖
+  const bgImg = await loadBgImage("/images/mb.png"); //<img src="...">
+
+  //遍歷每個canvas
+  for (const [index, src] of apngList.entries()) {
+    let canvasEl;
+    canvasEl = canvasRefs.value[index];
+    if (!canvasEl) return;
+
+    const ctx = canvasEl.getContext("2d", { willReadFrequently: true });
+    // ----------------------------------------------------------------
+    //設定背景畫布大小
+    // ----------------------------------------------------------------
+    canvasEl.width = bgImg.width;
+    canvasEl.height = bgImg.height;
+    // ----------------------------------------------------------------
+    // 設定影片畫布大小並準備播放器
+    // ----------------------------------------------------------------
+    const virtualCanvas = document.createElement("canvas");
+    const virtualCtx = virtualCanvas.getContext("2d", {
+      willReadFrequently: true,
+    });
+    const apng = await initApngPlayer(src, virtualCtx);
+    virtualCanvas.width = apng.width;
+    virtualCanvas.height = apng.height;
+    apng.numPlays = 2;
+    const player = await apng.getPlayer(virtualCtx);
+
+    // ----------------------------------------------------------------
+    // 我們使用 GSAP 的 ticker 來驅動畫面更新，這樣效能最好
+
+    ScrollTrigger.create({
+      trigger: canvasEl,
+      start: "top 50%", // 視窗的 50% 碰到元素的頂部時觸發 (等同 threshold 0.5)
+      end: "bottom top",
+      onEnter: () => {
+        player.play();
+        gsap.ticker.add(() => renderApng(bgImg, ctx, canvasEl, virtualCanvas));
+      },
+      onLeave: () => {
+        player.pause();
+        gsap.ticker.remove(() =>
+          renderApng(bgImg, ctx, canvasEl, virtualCanvas),
+        );
+      },
+      onLeaveBack: () => {
+        player.play();
+        gsap.ticker.add(() => renderApng(bgImg, ctx, canvasEl, virtualCanvas));
+      },
+      onEnterBack: () => {
+        player.play();
+        gsap.ticker.add(() => renderApng(bgImg, ctx, canvasEl, virtualCanvas));
+      },
+    });
   }
 });
 
+//------------------------------------------------------------------
+//準備apng播放器，回傳apng物件
+const initApngPlayer = async (url, ctx) => {
+  const imgBuffer = await getImgBuffer(url);
+  if (!imgBuffer) return;
+
+  const apng = parseAPNG(imgBuffer);
+  return apng;
+};
+
+//------------------------------------------------------------------
+//將apng轉換為純二進位數據`
+const getImgBuffer = async (url) => {
+  try {
+    const response = await fetch(url);
+    const buffer = await response.arrayBuffer(); //(純二進位數據)
+    return buffer;
+  } catch (error) {
+    console.error("圖片讀取失敗:", error);
+  }
+};
+
+//------------------------------------------------------------------
+// 載入圖片並回傳一個 Image 物件，後續被 Canvas.drawimge() 使用
+const loadBgImage = (src) => {
+  return new Promise((resolve, reject) => {
+    const img = new Image(); //web api 等同於 document.createElement("img")
+    img.src = src;
+    img.onload = () => resolve(img); // 圖片載入成功，回傳 Image 物件 給呼叫者(await)
+    img.onerror = (e) => reject(e);
+  });
+};
+
+//------------------------------------------------------------------
+//渲染
+const renderApng = (bgImg, ctx, canvasEl, virtualCanvas) => {
+  // 計算 APNG 在手機畫面中的位置
+  // ----------------------------------------------------------------
+  const originX = 20;
+  const originY = 50; // 20,50為畫布放置的起始點
+  const screenW = 300; // iphone螢幕比例約為2:1 所以apng比例也要是2:1 否則會變形
+  const screenH = 600;
+
+  // 1. 清空實體畫布
+  ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+  // 2. 先畫 APNG (把虛擬畫布貼上來)
+  // APNG 播放器會自動更新 virtualCanvas 的內容，我們只要把它貼過來就好
+  ctx.drawImage(virtualCanvas, originX, originY, screenW, screenH);
+  // ctx.drawImage(virtualCanvas, originX, originY);
+  // 3. 再畫背景 (手機框)
+  ctx.drawImage(bgImg, 0, 0, canvasEl.width, canvasEl.height);
+};
+
 window.onload = async function () {
-  const playerMap = new Map(); //可以儲存DOM的物件
-
-  for (const [index, src] of apngList.entries()) {
-    const canvasEl = document.getElementById(`canvas${index + 1}`);
-
-    if (canvasEl) {
-      const ctx = canvasEl.getContext("2d", { willReadFrequently: true });
-      // const apng = await initApngPlayer(src, ctx);
-
-      canvasEl.width = bgImg.width;
-      canvasEl.height = bgImg.height;
-
-      // 這是一個看不見的 Canvas，專門讓 APNG 在上面跑
-      const virtualCanvas = document.createElement("canvas");
-      const virtualCtx = virtualCanvas.getContext("2d", {
-        willReadFrequently: true,
-      });
-
-      // 初始化 APNG，注意這裡傳入的是 virtualCtx (虛擬的 context)
-      const apng = await initApngPlayer(src, virtualCtx);
-
-      // 設定虛擬畫布大小 = APNG 的大小
-      virtualCanvas.width = apng.width;
-      virtualCanvas.height = apng.height;
-
-      apng.numPlays = 2;
-      const player = await apng.getPlayer(virtualCtx);
-      playerMap.set(canvasEl, player);
-      // console.log(index);
-
-      // ----------------------------------------------------------------
-      // 步驟 C: 計算 APNG 在手機畫面中的位置 (參考您上一題的算法)
-      // ----------------------------------------------------------------
-      // 這裡請填入您計算出的實際像素座標
-      // 例如：手機圖 671px 高，top: 7.4% => 671 * 0.074 = 約 50px
-      const screenX = 20; // 範例值
-      const screenY = 50; // 範例值
-      const screenW = 302; // 範例值
-      const screenH = 600; // 範例值
-
-      // ----------------------------------------------------------------
-      // 步驟 D: 建立渲染迴圈 (合成器)
-      // ----------------------------------------------------------------
-      // 我們使用 GSAP 的 ticker 來驅動畫面更新，這樣效能最好
-      gsap.ticker.add(() => {
-        // 1. 清空實體畫布
-        ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
-
-        // 3. 再畫 APNG (把虛擬畫布貼上來)
-        // APNG 播放器會自動更新 virtualCanvas 的內容，我們只要把它貼過來就好
-        ctx.drawImage(virtualCanvas, screenX, screenY, screenW, screenH);
-
-        // 2. 先畫背景 (手機框)
-        ctx.drawImage(bgImg, 0, 0, canvasEl.width, canvasEl.height);
-      });
-
-      ScrollTrigger.create({
-        trigger: canvasEl,
-        start: "top 50%", // 視窗的 50% 碰到元素的頂部時觸發 (等同 threshold 0.5)
-        end: "bottom top",
-        onEnter: () => player.play(),
-        onLeave: () => player.pause(),
-        onLeaveBack: () => player.pause(),
-        onEnterBack: () => player.play(),
-      });
-    }
-  }
-
-  //------------------------------------------------------------------
-  //初始化apng播放器，回傳一個apng物件
-  async function initApngPlayer(url, ctx) {
-    const imgBuffer = await getImgBuffer(url);
-    if (!imgBuffer) return;
-
-    const apng = parseAPNG(imgBuffer);
-    return apng;
-  }
-
-  //------------------------------------------------------------------
-  //將apng轉換為純二進位數據`
-  async function getImgBuffer(url) {
-    try {
-      const response = await fetch(url);
-      const buffer = await response.arrayBuffer(); //(純二進位數據)
-      return buffer;
-    } catch (error) {
-      console.error("圖片讀取失敗:", error);
-    }
-  }
-
-  //------------------------------------------------------------------
-  // 載入圖片並回傳一個 Image 物件，後續被 Canvas.drawimge() 使用
-  function loadImage(src) {
-    return new Promise((resolve, reject) => {
-      const img = new Image(); //web api 等同於 document.createElement("img")
-      img.crossOrigin = "anonymous"; // 若圖片跨域需要加這行
-      img.onload = () => resolve(img); // 圖片載入成功，回傳 Image 物件 給呼叫者(await)
-      img.onerror = (e) => reject(e);
-      img.src = src;
-    });
-  }
+  // for (const [index, src] of apngList.entries()) {
+  //   const canvasEl = document.getElementById(`canvas${index + 1}`);
+  //
+  // }
 };
 </script>
 
