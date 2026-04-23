f2ea51e40343fb275f35ab99123cff2edced5dff
emily <emily929109@gmail.com>
2026-04-23 23:02:03 +0800

    Reaction Timer functionality with Block component

---
+++ 22_reaction-timer/src/components/Block.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper"></div>
+</template>
+
+<script>
+export default {
+  name: "Block",
+  props: {
+    delay: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
+</style>
